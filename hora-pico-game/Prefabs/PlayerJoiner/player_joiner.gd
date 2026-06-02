4c4e7275017318ff150ac509fc09dfa0b59c59cf
Daniel Penedo <danielpenedo2@gmail.com>
2026-06-01 19:08:40 -0600

    feat: Traffic Player can now hack traffic lights

--- hora-pico-game/Prefabs/PlayerJoiner/player_joiner.gd
+++ hora-pico-game/Prefabs/PlayerJoiner/player_joiner.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @export var player_prefab: PackedScene
+@export var traffic_player_prefab: PackedScene
 @export var pointer_textures: Array[Texture2D]
 
 var controllers: Dictionary = {}
@@ -11,13 +12,21 @@ func _ready() -> void:
 
 func _on_joy_connection_changed(device: int, connected: bool):
 	player_index += 1
-	var node: PlayerPointer = player_prefab.instantiate()
-	node.device_id = device
-	node.texture = pointer_textures[player_index % len(pointer_textures)]
-	add_child(node)
-	if connected:
-		controllers.set(device, node)
-	elif controllers.erase(device):
-		print("Player %d disconnected!" % device)
+	var node = null
+	
+	if player_index == 0:
+		node = traffic_player_prefab.instantiate()
+	
 	else:
-		print("Player %d disconnected, but no device erased!" % device)
+		node = player_prefab.instantiate()
+	
+	if node != null:
+		node.device_id = device
+		node.texture = pointer_textures[player_index % len(pointer_textures)]
+		add_child(node)
+		if connected:
+			controllers.set(device, node)
+		elif controllers.erase(device):
+			print("Player %d disconnected!" % device)
+		else:
+			print("Player %d disconnected, but no device erased!" % device)
