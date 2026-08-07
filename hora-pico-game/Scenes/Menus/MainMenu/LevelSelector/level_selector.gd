bc17863d71907d424b6416fa08c30ad2fd579ffb
Daniel Penedo <danielpenedo2@gmail.com>
2026-08-07 16:19:04 -0600

    feat: add control support in level selector

--- hora-pico-game/Scenes/Menus/MainMenu/LevelSelector/level_selector.gd
+++ hora-pico-game/Scenes/Menus/MainMenu/LevelSelector/level_selector.gd
@@ -4,12 +4,21 @@ extends MarginContainer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("left_traffic"):
+		_on_level_1_button_pressed()
+		
+	if event.is_action_pressed("up_traffic"):
+		_on_level_2_button_pressed()
+		
+	if event.is_action_pressed("right_traffic"):
+		_on_level_3_button_pressed()
 
 func _on_level_1_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Level1.tscn")
